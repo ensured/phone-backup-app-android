8e22e731e0077326315e1929c5248bab1583b703
ensured <74529637+ensured@users.noreply.github.com>
2024-09-30 15:05:12 -0700

    remove

--- app/page.js
+++ app/page.js
@@ -1,5 +1,4 @@
 import { startAdbServer } from "@/actions/_actions";
-import { CheckIcon } from "lucide-react";
 import dynamic from "next/dynamic";
 
 const Chat = dynamic(() => import("../app/components/chat"), { ssr: false });
